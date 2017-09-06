988496545ee60f46660ec6d00b54aacef7b87608
BG2CRW <xuchaofan1994@hotmail.com>
2017-09-06 13:20:45 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 passenger_flow_statistics
+
+cd client2.0
+mkdir build
+cd build
+cmake ..
+make
+
+
+cd cleint_shell
+mkdir build
+cd build
+qmake ..
+make
+
---
+++ client2.0/build/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/client.dir/src/main.cpp.o"
+  "CMakeFiles/client.dir/src/comparision.cpp.o"
+  "CMakeFiles/client.dir/src/trackpoint.cpp.o"
+  "CMakeFiles/client.dir/src/matqueue.cpp.o"
+  "CMakeFiles/client.dir/src/SocketMatTransmissionClient.cpp.o"
+  "client.pdb"
+  "client"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
